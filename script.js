1f5d621fb1a5de749b0cb99c252948fe4b2680ed
Daniel Kreck <Daniel_Kreck@comcast.com>
2022-07-26 16:47:53 -0400

    Added 'All fields required' alert

--- script.js
+++ script.js
@@ -11,3 +11,40 @@
 </ul>
 <img src="${}">
 */
+
+let userInputs = [];
+  
+window.addEventListener("load", function(){
+   launchForm.addEventListener("submit", function(event) {
+      userInputs.push(document.querySelector("input[name=pilotName]"));
+      userInputs.push(document.querySelector("input[name=copilotName]"));
+      userInputs.push(document.querySelector("input[name=fuelLevel]"));
+      userInputs.push(document.querySelector("input[name=cargoMass]"));
+      
+      for(let i=0; i<userInputs.length; i++){
+         console.log(userInputs[i].value);
+         if(!userInputs[i].value ){
+            alert("All fields are required!");
+            event.preventDefault();
+            break;
+         }
+      }
+
+   });
+
+});
+// Validation
+// setup a window load handler ~~~~~~~~~~
+   //set up submit handler for the form ~~~~~~~~~~~~
+
+      //call event.preventDefault() to cancel submission ~~~~~~~~~
+      
+      //Grab formgroup values, if they are empty: alert ~~~~~~~~~~~~~~~
+
+      //Check fuel and mass to be numbers 
+      //check the values, and report launch status
+      // Make the list visible
+
+   //fetch planet data
+      //randomly choose one of the planets
+      //display random planet's info (like above) push it to top container
